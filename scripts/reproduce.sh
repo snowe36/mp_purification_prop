b80d385c20c30ed6384851cb495793205e598878
snowe36 <eric.snow36@gmail.com>
2026-09-11 21:52:19 -0400

    hook in the detergent information

--- scripts/reproduce.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-export MPLCONFIGDIR="${MPLCONFIGDIR:-.mplconfig}"
-mpx-demo
-pytest -q
--- src/mpatlas/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Membrane-protein HTS atlas."""
-
-from __future__ import annotations
-
-__version__ = "0.1.0"
